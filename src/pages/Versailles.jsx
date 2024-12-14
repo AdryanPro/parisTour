6eb2642e0a46ae580947d450047e17a4c02aaef0
AdryanPro <adryan-pro@hotmail.com>
2024-12-14 17:10:57 +0100

    Versailles Responsive done

--- src/pages/Versailles.jsx
+++ src/pages/Versailles.jsx
@@ -200,13 +200,13 @@ export default function Versailles() {
         <div className="carrousel">
           <div className="item">
             <img src={slides[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="VersaillesImg" />
+            <button className='SliderImgBtnVersailles Prev' onClick={prevSlide}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="30px" width="30px" className='VersaillesSvg'><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+            </button>
+            <button className='SliderImgBtnVersailles Next' onClick={nextSlide}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="30px" width="30px" className='VersaillesSvg'><path fill="#ffffff" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            </button>
           </div>
-          <button className='SliderImgBtnVersailles Prev' onClick={prevSlide}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="30px" width="30px" className='VersaillesSvg'><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
-          </button>
-          <button className='SliderImgBtnVersailles Next' onClick={nextSlide}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="30px" width="30px" className='VersaillesSvg'><path fill="#ffffff" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
-          </button>
         </div>
 
         <div className="VersaillesPaymentCard" data-aos-duration="1600" data-aos="fade-down" data-aos-delay="1000">
@@ -338,13 +338,13 @@ export default function Versailles() {
         <div className="carrouselMobile">
           <div className="item">
             <img src={slidesMobile[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="VersaillesMobileCarouselSlide" />
-          </div>
-          <button className='SliderImgBtnVersaillesMobile PrevMobile' onClick={prevSlide}>
+            <button className='SliderImgBtnVersaillesMobile PrevMobile' onClick={prevSlide}>
             {"<"}
-          </button>
-          <button className='SliderImgBtnVersaillesMobile NextMobile' onClick={nextSlide}>
-            {">"}
-          </button>
+            </button>
+            <button className='SliderImgBtnVersaillesMobile NextMobile' onClick={nextSlide}>
+              {">"}
+            </button>
+          </div>
         </div>
         <div className="VersaillesMobileCardShowAndBuyTxt">
           <p className='MobileCardTitle'>Number of  People :</p>
